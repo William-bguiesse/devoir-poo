9a4e721dc224b5fb2351db0112d93be6c553cbcf
Willi37 <127964133+Willi37@users.noreply.github.com>
2026-02-13 14:26:45 +0100

    Bguiesse 04-serialisation-client-vehicule

--- models/client.py
+++ models/client.py
@@ -1,7 +1,7 @@
 class Client:
     def __init__(self, id_client, nom, prenom, mail, telephone, adresse):
         self.id_client = id_client
-        self.nom = nom
+        self.nom = str(nom)
         self.prenom = prenom
         self.mail = mail
         self.telephone = telephone
@@ -9,3 +9,17 @@ class Client:
 
     def __str__(self):
         return f"{self.nom} {self.prenom} ({self.mail})"
+    
+    def to_dict(self):
+        return {
+            "id_client": self.id_client,
+            "nom": self.nom,
+            "prenom": self.prenom,
+            "mail": self.mail,
+            "telephone": self.telephone,
+            "adresse": self.adresse
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
--- models/vehicule.py
+++ models/vehicule.py
@@ -1,6 +1,6 @@
 class Vehicule:
     def __init__(self, id_vehicule, marque, modele, cylindree, kilometrage_actuel, date_mise_en_circulation):
-        self.id_vehicule = id_vehicule
+        self.id_vehicule = str(id_vehicule)
         self.marque = marque
         self.modele = modele
         self.cylindree = int(cylindree)
@@ -9,3 +9,19 @@ class Vehicule:
 
     def __str__(self):
         return f"{self.marque} {self.modele} ({self.cylindree} cyl., {self.kilometrage_actuel} km)"
+    
+    def to_dict(self):
+        return {
+            "id_vehicule": self.id_vehicule,
+            "marque": self.marque,
+            "modele": self.modele,
+            "cylindree": self.cylindree,
+            "kilometrage_actuel": self.kilometrage_actuel,
+            "date_mise_en_circulation": self.date_mise_en_circulation
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
+    
+    
