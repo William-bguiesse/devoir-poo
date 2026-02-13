b8444c09eadbd1b0bcf0e94dcf679adb6c6f571e
Willi37 <127964133+Willi37@users.noreply.github.com>
2026-02-13 15:17:14 +0100

    Bguiesse 11-reservation-calcul-cout-estime

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from models.vehicule import Vehicule
 from models.reservation import Reservation
 from models.tarifs import TarifsManager
 from data_manager import DataManager
+import ui
 
 def main():
     dm = DataManager()
@@ -12,7 +13,7 @@ def main():
     reservations = dm.charger("reservations.json", Reservation)
 
     while True:
-        print("\n--- GESTION LOCA-AUTO (Étape 8) ---")
+        print("\n--- GESTION LOCA-AUTO ---")
         print("1. Ajouter un client")
         print("2. Ajouter un véhicule")
         print("3. Créer une réservation")
@@ -31,31 +32,37 @@ def main():
         elif choix == "2":
             id_v = input("ID Véhicule : ")
             marque = input("Marque : ")
-            cyl = int(input("Cylindrée (4, 5 ou 6) : "))
-            vehicules.append(Vehicule(id_v, marque, "Modèle", cyl, 0.0, "2024"))
-            print("Véhicule ajouté.")
+            try:
+                cyl = int(input("Cylindrée (4, 5 ou 6) : "))
+                vehicules.append(Vehicule(id_v, marque, "Modèle", cyl, 0.0, "2024"))
+                print("Véhicule ajouté !")
+            except ValueError:
+                print("Erreur : La cylindrée doit être un nombre !")
 
         elif choix == "3":
             id_res = input("ID Réservation : ")
             id_c = input("ID du Client : ")
             id_v = input("ID du Véhicule : ")
+            
             c_obj = next((c for c in clients if c.id_client == id_c), None)
             v_obj = next((v for v in vehicules if v.id_vehicule == id_v), None)
             
             if c_obj and v_obj:
-                j = int(input("Nombre de jours : "))
-                k = int(input("Forfait KM : "))
-                res = Reservation(id_res, id_c, id_v, j, k, tm, v_obj)
-                reservations.append(res)
-                print(f"Réservation créée ! Coût : {res.cout_estime}€")
+                try:
+                    j = int(input("Nombre de jours : "))
+                    k = int(input("Forfait KM : "))
+                    res = Reservation(id_res, id_c, id_v, j, k, tm, v_obj)
+                    reservations.append(res)
+                    print(f"Réservation créée. Coût : {res.cout_estime}€")
+                except ValueError:
+                    print("Erreur : Jours et KM doivent être des nombres !")
             else:
                 print("Erreur : ID Client ou Véhicule inconnu.")
 
         elif choix == "4":
-            print("\n--- TOUTES LES DONNÉES ---")
-            for c in clients: print(c)
-            for v in vehicules: print(v)
-            for r in reservations: print(r)
+            ui.afficher_clients(clients)
+            ui.afficher_vehicules(vehicules)
+            ui.afficher_reservations(reservations)
 
         elif choix == "5":
             dm.sauvegarder("clients.json", clients)
